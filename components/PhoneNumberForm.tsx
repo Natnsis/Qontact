1bea4a3742b4d9f4308f25d82b9e927351fe537c
Nati <nsisay49@gmail.com>
2026-02-25 15:07:17 +0300

    phone number form

--- components/PhoneNumberForm.tsx
+++ components/PhoneNumberForm.tsx
@@ -1,5 +1,9 @@
 import { colors } from "@/constants/color"
 import { View, Text } from "react-native"
+import { Label } from "@/components/ui/label"
+import { Input } from "@/components/ui/input"
+import { Button } from "./ui/button"
+
 const PhoneNumberForm = () => {
   return (
     <View>
@@ -7,12 +11,41 @@ const PhoneNumberForm = () => {
         style={{
           color: colors.light,
           fontFamily: 'regular',
-          fontSize: 18
+          fontSize: 20
         }}
-        className="">Phone number infos</Text>
-      <Text>all data will be saved locally, and share whenever you choose</Text>
-      <View>
+      >Phone number infos</Text>
 
+      <View className="mt-2 mx-2 p-2">
+        <Label
+          style={{
+            color: colors.light,
+            fontFamily: 'light',
+            fontSize: 12
+          }}
+        >Full Name / Nick Name</Label>
+        <Input style={{ fontFamily: 'light' }} className="border-[#96dded] mb-5" />
+
+        <Label
+          style={{
+            color: colors.light,
+            fontFamily: 'light',
+            fontSize: 12
+          }}
+        >
+          Phone Number
+        </Label>
+        <Input
+          style={{ fontFamily: 'light' }}
+          className="border-[#96dded] mb-5"
+          keyboardType="numeric" />
+
+        <View className="flex-row justify-end">
+          <Button style={{ backgroundColor: colors.secondary }}>
+            <Text style={{ color: colors.dark, fontFamily: 'regular' }}>
+              Save
+            </Text>
+          </Button>
+        </View>
       </View>
     </View>
   )
