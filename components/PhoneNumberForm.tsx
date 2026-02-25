c56a23eb1ed58f40437d7505dd8eb0786526bd6e
Nati <nsisay49@gmail.com>
2026-02-25 16:03:31 +0300

    phone number form with data

--- components/PhoneNumberForm.tsx
+++ components/PhoneNumberForm.tsx
@@ -3,8 +3,18 @@ import { View, Text } from "react-native"
 import { Label } from "@/components/ui/label"
 import { Input } from "@/components/ui/input"
 import { Button } from "./ui/button"
+import { Feather } from '@expo/vector-icons';
 
 const PhoneNumberForm = () => {
+  const contacts = [
+    { id: '0', name: 'All Contacts', time: '30-01-24', phone: '#*********#' },
+    { id: '1', name: 'Natnael Sisay', time: '30-01-24', phone: '+251911223344' },
+    { id: '2', name: 'Sara Belay', time: '02-02-24', phone: '+251911556677' },
+    { id: '3', name: 'Dawit Isaac', time: '15-02-24', phone: '+251920889900' },
+    { id: '4', name: 'Elias Tekle', time: '20-02-24', phone: '+251944112233' },
+    { id: '5', name: 'Marta Hailu', time: '22-02-24', phone: '+251912004455' },
+    { id: '6', name: 'Yonas Alemu', time: '24-02-24', phone: '+251930778899' },
+  ];
   return (
     <View>
       <Text
@@ -45,6 +55,52 @@ const PhoneNumberForm = () => {
               Save
             </Text>
           </Button>
+        </View>
+      </View>
+
+      <View>
+        <Text
+          style={{
+            color: colors.primary,
+            fontFamily: 'regular',
+            fontSize: 16
+          }}>
+          Saved Numbers
+        </Text>
+
+        <View>
+          {contacts.map((c, index) => (
+            <View key={index} className="mb-2">
+              <View className="flex-row items-center gap-2">
+                <Feather
+                  name='phone'
+                  color={colors.dark}
+                  style={{ backgroundColor: colors.secondary }}
+                  className="p-2 rounded-full" />
+                <Text
+                  style={{ color: colors.light, fontFamily: 'regular' }}>
+                  {c.name}
+                </Text>
+              </View>
+              <View className="flex-row gap-2 items-center justify-between">
+                <View>
+                  <Text
+                    style={{ color: colors.light, fontFamily: 'light' }}>
+                    {c.time}
+                  </Text>
+                </View>
+                <View className="flex-row items-center gap-2">
+                  <Text
+                    style={{ color: colors.light, fontFamily: 'regular' }}>
+                    {c.phone}
+                  </Text>
+                  <Button size='icon' variant='destructive'>
+                    <Feather name="trash" />
+                  </Button>
+                </View>
+              </View>
+            </View>
+          ))}
         </View>
       </View>
     </View>
