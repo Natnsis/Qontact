82892730dbb59f5bfa0387126e7e192d64927eda
Nati <nsisay49@gmail.com>
2026-02-27 16:49:52 +0300

    build configs

--- constants/numbers.ts
+++
@@ -1,7 +0,0 @@
-const numbers = [
-  { 'name': 'Natnael Sisay', 'phone': '+251903259444' },
-  { 'name': 'Alemu Kassahun', 'phone': '+251903480982' },
-  { 'name': 'Gebremariam Ayelign', 'phone': '+25190328032' },
-  { 'name': 'Minale Awor', 'phone': '+25193098230' },
-  { 'name': 'Daniel Balkew', 'phone': '+2519324093' },
-]  
